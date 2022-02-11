2f5be0a28a11eec5b53bbe6255922df334d0f3a6
Kashkanov <aquinokarl17@gmail.com>
2022-02-11 17:59:03 +0800

    console logs for testing

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function() {
     // Convert input based on selected method
     $('#convertButton').on('click', function() {
         var inputElement = $('#input-value');
@@ -13,7 +13,7 @@ $(document).ready(function(){
         var expcont;
         var exp = 0;
         var significand;
-       
+
         $(outputElement).css('color', 'white');
 
         if (input == '') {
@@ -24,73 +24,76 @@ $(document).ready(function(){
         let pattern = /e/;
         let pattern1 = /E/;
         console.log(pattern.test(input.toString()));
-        if (pattern.test(input.toString())){
+        if (pattern.test(input.toString())) {
             var array = input.split('e');
             significand = array[0];
             console.log(significand);
             exp = parseInt(array[1]);
             console.log("exps1");
             console.log(exp);
-        }
-        
-        else if (pattern1.test(input.toString())){
+        } else if (pattern1.test(input.toString())) {
             var array = input.split('E');
             significand = parseInt(array[0]);
             console.log(array[0]);
             exp = parseInt(array[1]);
             console.log("exps2");
             console.log(exp);
-        }
-
-        else{
+        } else {
             significand = input;
         }
 
         signbit = get_sign_bit(input);
-        if(signbit == 1){
+        if (signbit == 1) {
             significand = significand.substring(1);
             significand = parseInt(significand);
-     
+
         }
 
         console.log(significand);
-        while(significand.toString().includes('.')){
+        while (significand.toString().includes('.')) {
             significand = significand * 10;
             console.log(significand);
             exp = exp - 1;
         }
 
-        while(significand.toString().length < 7){
+        while (significand.toString().length < 7) {
             significand = significand * 10;
             exp = exp - 1;
         }
-        
+
         /*5.673459e6*/
         significand = significand.toString();
         tempMSD = significand[0];
         tempbcd = significand.substring(1);
 
-        switch(method) {
+        switch (method) {
             case "NR":
                 console.log(tempbcd);
+                console.log("sign = " + signbit); //<===
                 tempbcd = densely_fixer(tempbcd);
                 expcont = get_exponent_continuation(exp);
-                
-                combifield = combination_field(expcont,tempMSD);
+                console.log("e' = " + expcont); //<===
+                combifield = combination_field(expcont, tempMSD);
+                console.log("combi = " + combifield); //<===
                 expcont = expcont.substring(2);
+                console.log("exp cont. = " + expcont); //<===
                 $(outputElement).text(signbit + " " + combifield + " " + expcont + " " + tempbcd)
                 break;
-                
+
             case "RTNE":
                 console.log('Option2');
                 newSignificand = round_to_nearest_even(significand);
                 tempMSD = newSignificand[0];
                 tempbcd = newSignificand.substring(1);
+                console.log("sign = " + signbit); //<===
 
                 tempbcd = densely_fixer(tempbcd);
                 expcont = get_exponent_continuation(exp);
-                combifield = combination_field(expcont,tempMSD);
+                console.log("e' = " + expcont);
+                combifield = combination_field(expcont, tempMSD);
+                console.log("combi = " + combifield); //<===
                 expcont = expcont.substring(2);
+                console.log("exp cont. = " + expcont); //<===
                 $(outputElement).text(signbit + " " + combifield + " " + expcont + " " + tempbcd);
                 break;
 
